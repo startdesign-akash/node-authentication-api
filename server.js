020fb48c97be45c3a331ae3ccf3351490fae476b
akashgundaniya <[akashwork93@gmail.com]>
2023-01-13 11:28:30 +0530

    CROS URL update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dbConfig = require("./app/config/db.config");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "https://node-demo-auth.onrender.com"
 };
 
 app.use(cors(corsOptions));
